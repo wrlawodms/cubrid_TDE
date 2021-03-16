a5d4f104599a8bee3174b510dd1398ea37c64854
bigs <wrlawodms@gmail.com>
2021-03-16 16:05:21 +0900

    Chnage license headers

--- src/storage/heap_file.c
+++ src/storage/heap_file.c
@@ -2,7 +2,6 @@
  * Copyright 2008 Search Solution Corporation
  * Copyright 2016 CUBRID Corporation
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
